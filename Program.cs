3c51c861944fd939915c2aa698b71f085f02895e
sbvyas <shivanivyas2008@gmail.com>
2020-06-25 01:13:03 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class JumpingOnTheCloud
+{		
+	static int jumpingOnClouds(int[] c)
+	{
+		int aTotalSteps = 0;
+		
+		for (int i = 0; i < c.Length - 1; i++)
+		{
+			if (i == c.Length - 2)
+			{
+				aTotalSteps++;
+				break;
+			}
+			if (c[i + 2] == 0)
+			{
+				i++;
+			}
+			aTotalSteps++;
+		}
+		return aTotalSteps;
+	}
+
+	static void Main(string[] args)
+	{		
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp));
+		int result = jumpingOnClouds(c);
+		Console.WriteLine(result.ToString());
+		Console.ReadLine();		
+	}
+}
